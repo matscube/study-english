714c44dcbb136e77f62ec077285b86c2551e7917
Takanori Matsumoto <matscube@gmail.com>
2024-07-19 01:40:24 +0900

    refactor(index.ts): move TTS functions and text data to a new tts.ts file for better modularity and separation of concerns

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,6 @@
-import fs from "fs";
-import OpenAI from "openai";
-import "dotenv/config";
 import { createVideoFromImage } from "./video";
 import { concatAudios } from "./audio";
 
-const openai = new OpenAI();
-
-async function ttsJapanese(text: string, output: string) {
-  const mp3 = await openai.audio.speech.create({
-    model: "tts-1",
-    voice: "fable",
-    input: text,
-  });
-  const buffer = Buffer.from(await mp3.arrayBuffer());
-  await fs.promises.writeFile(output, buffer);
-}
-async function ttsEnglish(text: string, output: string) {
-  const mp3 = await openai.audio.speech.create({
-    model: "tts-1",
-    voice: "alloy",
-    input: text,
-  });
-  const buffer = Buffer.from(await mp3.arrayBuffer());
-  await fs.promises.writeFile(output, buffer);
-}
-const texts: { ja: string; en: string }[] = [
-  {
-    ja: "会議室を押さえておいてくれますか？",
-    en: "Could you reserve a meeting room?",
-  },
-  {
-    ja: "本日の会議の議題をメールにて添付しましたので、事前にご確認下さいませ。",
-    en: "I have attached the agenda for today’s meeting in this e-mail. Please have a look at it in advance.",
-  },
-  {
-    ja: "会議についていけず、議事録がとれませんでした。",
-    en: "I couldn’t keep up with the meeting and couldn’t take the minutes.",
-  },
-  {
-    ja: "電話会議をさせていただけますか？",
-    en: "Would it be possible for us to have a teleconference?",
-  },
-];
-async function main() {
-  texts.forEach(async (text, index) => {
-    const outputJapanese = `./out/speech-jp-${index}.mp3`;
-    const outputEnglish = `./out/speech-en-${index}.mp3`;
-    await ttsJapanese(text.ja, outputJapanese);
-    await ttsEnglish(text.en, outputEnglish);
-  });
-}
-// main();
 
 // createVideoFromImage();
 concatAudios();
