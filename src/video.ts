32c6b05cff988a1a6a147628149be152747da406
Takanori Matsumoto <matscube@gmail.com>
2024-07-24 21:35:41 +0900

    feat(audio.ts): make concatAudios function asynchronous and accept dynamic input feat(index.ts): integrate TTS and video creation functions into a single workflow feat(tts.ts): export TTS functions and update to use props for input feat(video.ts): make mixAudioAndVideo and concatVideos functions asynchronous and accept dynamic input

--- src/video.ts
+++ src/video.ts
@@ -1,18 +1,18 @@
-const ffmpeg = require('fluent-ffmpeg');
+const ffmpeg = require("fluent-ffmpeg");
 export async function createVideoFromImage(props: {
   imagePath: string;
   outputPath: string;
 }): Promise<boolean> {
-  const durationSecond = 2
+  const durationSecond = 2;
   return new Promise((resolve, reject) => {
     ffmpeg(props.imagePath)
       .loop(durationSecond)
       .fps(25)
-      .on('end', function() {
+      .on("end", function () {
         resolve(true);
       })
-      .on('error', function(err: any) {
-        console.error('an error happened: ' + err.message);
+      .on("error", function (err: any) {
+        console.error("an error happened: " + err.message);
         reject(err);
       })
       // save to file
@@ -22,34 +22,53 @@ export async function createVideoFromImage(props: {
   });
 }
 
-export function mixAudioAndVideo() {
-  const videoPath = 'out/image-video-2.mp4';
-  const audioPath = 'out/speech-en-1.mp3';
-  const outputPath = 'out/output-1-en.mp4';
-  
-  ffmpeg()
-  .addInput(videoPath)
-  .addInput(audioPath)
-  .addOptions(['-map 0:v', '-map 1:a', '-c:v copy'])
-  .format('mp4')
-  .on('error', (error: any) => console.log(error))
-  .on('end', () => console.log(' finished !'))
-  .saveToFile(outputPath)
+export async function mixAudioAndVideo(props: {
+  videoPath: string;
+  audioPath: string;
+  outputPath: string;
+}): Promise<boolean> {
+  // const videoPath = 'out/image-video-2.mp4';
+  // const audioPath = 'out/speech-en-1.mp3';
+  // const outputPath = 'out/output-1-en.mp4';
+  return new Promise((resolve, reject) => {
+    ffmpeg()
+      .addInput(props.videoPath)
+      .addInput(props.audioPath)
+      .addOptions(["-map 0:v", "-map 1:a", "-c:v copy"])
+      .format("mp4")
+      .on("error", (error: any) => {
+        console.log(error);
+        reject(error);
+      })
+      .on("end", () => {
+        console.log(" finished !");
+        resolve(true);
+      })
+      .saveToFile(props.outputPath);
+  });
 }
 
-export function concatVideos() {
-  const videoPaths = [
-    'out/output-1-jp.mp4',
-    'out/output-1-en.mp4',
-  ];
-  const outputPath = 'out/output.mp4';
-  
+export async function concatVideos(props: {
+  videoPaths: string[];
+  outputPath: string;
+}): Promise<boolean> {
+  // const videoPaths = ["out/output-1-jp.mp4", "out/output-1-en.mp4"];
+  // const outputPath = "out/output.mp4";
+
   const command = ffmpeg();
-  videoPaths.forEach((videoPath) => {
+  props.videoPaths.forEach((videoPath) => {
     command.input(videoPath);
   });
-  command
-  .on('error', (error: any) => console.log(error))
-  .on('end', () => console.log(' finished !'))
-  .mergeToFile(outputPath);
+  return new Promise((resolve, reject) => {
+    command
+      .on("error", (error: any) => {
+        console.log(error);
+        reject(error);
+      })
+      .on("end", () => {
+        console.log(" finished !");
+        resolve(true);
+      })
+      .mergeToFile(props.outputPath);
+  });
 }
